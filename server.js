e2e40d18893c58c4f9227dbe1ae24a54228e24c3
Mira Heckmann <miraheckmann@gmail.com>
2021-01-06 18:46:12 -0800

    installs and commits cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 
 require('dotenv').config();
@@ -11,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(logger('dev'));
-//app.use(cors());
+app.use(cors());
 
 app.use('/api/users', require('./routes/api/users'));
 const port = process.env.PORT || 3001; 
